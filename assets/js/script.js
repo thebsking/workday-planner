7b943a9e90576659a14c302fb9dc9794c983a2c2
Brandon King <brandonscottking@gmail.com>
2021-02-20 22:39:01 -0500

    first run at color coding blocks; not working as expected

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,5 +8,21 @@ todaysDate.text(moment().format('dddd, MMMM Do'));
 
 //display time blocks
 for (var i = 0; i < hours.length; i++) {
-    mainArea.append($('<div>').addClass('row').text(hours[i]))
+    mainArea.append($('<div>').addClass('row').attr('id', 'hour-'+i).text(hours[i]))
 };
+
+
+//color code time rows
+let rowNumber = mainArea.children().length;
+for (var x = 0; x < rowNumber; x++) {
+    currentRow = $(`#hour-${x}`);
+    if (currentRow.text() > moment().format('hA')) {
+       currentRow.addClass(".past");
+    } else if (currentRow < moment().format('hA')){
+        currentRow.addClass(".future");
+    } else {
+        currentRow.addClass(".present")
+    }
+}
+
+// 
