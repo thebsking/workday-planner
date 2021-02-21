4f09023b1e36247b8bb1d62de22ce9f8a760c75c
Brandon King <brandonscottking@gmail.com>
2021-02-21 17:19:21 -0500

    refactored display for loop to nest all elements correctly

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,21 +8,27 @@ todaysDate.text(moment().format('dddd, MMMM Do'));
 
 //display time blocks
 for (var i = 0; i < hours.length; i++) {
-    mainArea.append($('<div>').addClass('row').attr('id', 'hour-'+i).text(hours[i]))
+   mainArea.append($('<div>').addClass('time-block').attr('id', 'hour-'+i));
+   $('div.time-block').eq(i).append($('<div>').addClass('row'))
+   $('div.row').eq(i).append($('<span>').addClass('hour').text(hours[i])).append($('<textarea>').addClass('description')).append($('<button>').addClass('saveBtn'))
 };
+// let rowNumber = mainArea.children().length;
+// for (var i = 0; i < rowNumber; i++){
+//     $('div.time-block:eq(i)').append($('<div>').addClass('row'))
+// }
 
 
 //color code time rows
-let rowNumber = mainArea.children().length;
-for (var x = 0; x < rowNumber; x++) {
-    currentRow = $(`#hour-${x}`);
-    if (currentRow.text() > moment().format('hA')) {
-       currentRow.addClass(".past");
-    } else if (currentRow < moment().format('hA')){
-        currentRow.addClass(".future");
-    } else {
-        currentRow.addClass(".present")
-    }
-}
+
+// for (var x = 0; x < rowNumber; x++) {
+//     currentRow = $(`#hour-${x}`);
+//     if (currentRow.text() > moment().format('hA')) {
+//        currentRow.addClass(".past");
+//     } else if (currentRow < moment().format('hA')){
+//         currentRow.addClass(".future");
+//     } else {
+//         currentRow.addClass(".present")
+//     }
+// }
 
 // 
