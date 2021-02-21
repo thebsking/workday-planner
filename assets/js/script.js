941b5e6e434bf9906a129c086dc1385e44b6b43f
Brandon King <brandonscottking@gmail.com>
2021-02-20 22:13:18 -0500

    add for loop to display time blocks

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,12 @@
 //declare variables
 let todaysDate = $('#currentDay');
+let mainArea = $('#main-content');
 let hours = ['9AM', '10AM', '11AM', '12PM', '1PM', '2PM', '3PM', '4PM', '5PM'];
-
 
 //display current date
 todaysDate.text(moment().format('dddd, MMMM Do'));
+
+//display time blocks
+for (var i = 0; i < hours.length; i++) {
+    mainArea.append($('<div>').addClass('row').text(hours[i]))
+};
