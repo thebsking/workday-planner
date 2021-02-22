75a4fe3f1917ca1ca64ab7e77229f81585e6cb06
Brandon King <brandonscottking@gmail.com>
2021-02-21 19:26:21 -0500

    added event listner for save buttons; not saving correct element

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,19 +10,25 @@ todaysDate.text(moment().format('dddd, MMMM Do'));
 for (var i = 0; i < hours.length; i++) {
    mainArea.append($('<div>').addClass('time-block').attr('id', 'hour-'+i));
    $('div.time-block').eq(i).append($('<div>').addClass('row'))
-   $('div.row').eq(i).append($('<span>').addClass('hour').text(hours[i])).append($('<textarea>').addClass('description')).append($('<button>').addClass('saveBtn'))
+   $('div.row').eq(i).append($('<span>').addClass('hour').text(hours[i]).css('padding-top', '5px')).append($('<textarea>').addClass('description')).append($('<button>').addClass('saveBtn').text('save'))
 };
 
 //color code time rows
 let rowCount = mainArea.children().length;
 for (var x = 0; x < rowCount; x++) {
     let currentRow = $(`#hour-${x} > div > span`);
-    if (currentRow.text() > moment().format('hA')) {
+    if (currentRow.text() < moment().format('hA')) {
        currentRow.next($('textarea')).addClass("past");
-    } else if (currentRow < moment().format('hA')){
+    } else if (currentRow .text() > moment().format('hA')){
         currentRow.next($('textarea')).addClass("future");
     } else {
-        currentRow.next($('textarea')).addClass("present")
+        currentRow.next($('textarea')).addClass("present");
     }
-}
+};
 
+//event listener to save input
+$('.saveBtn').on('click', function (event){
+    event.preventDefault();
+    let textInput = $(this).siblings().text();
+    window.localStorage.setItem('savedItem', textInput)
+})
